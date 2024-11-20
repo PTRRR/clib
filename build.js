38681aeec8e30028e16d7da52d2a33f76f935d4b
PTRRR <pietro.alberti@bluewin.ch>
2024-11-20 17:26:28 +0100

    Update build process

---
+++ build.js
@@ -0,0 +1,25 @@
+// build.js
+const esbuild = require("esbuild");
+const path = require("path");
+
+esbuild
+  .build({
+    entryPoints: [path.resolve(__dirname, "lib/index.ts")],
+    outdir: "dist",
+    bundle: true,
+    format: "esm",
+    platform: "neutral",
+    outExtension: { ".js": ".js" },
+    minify: false,
+    sourcemap: false,
+    target: ["es2020"],
+    outbase: "lib",
+    loader: {
+      ".ts": "ts",
+      ".js": "js",
+    },
+    resolveExtensions: [".ts", ".js", ".json"],
+    mainFields: ["module", "main"],
+    tsconfig: path.resolve(__dirname, "./tsconfig.json"),
+  })
+  .catch(() => process.exit(1));
