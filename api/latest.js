fb430b4feae97acf52981fe8dd84bc69506d3971
PTRRR <pietro.alberti@bluewin.ch>
2024-12-03 16:28:11 +0100

    Release v1.0.8

--- api/latest.js
+++ api/latest.js
@@ -3,6 +3,14 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const packageJson = JSON.parse(
     readFileSync(join(process.cwd(), "package.json"), "utf8")
   );
