b65e481e367764051e48602c2e47d469fc641b26
PTRRR <pietro.alberti@bluewin.ch>
2024-12-03 15:29:28 +0100

    Add more examples

--- examples/main.ts
+++ examples/main.ts
@@ -1,12 +1,32 @@
-import { createClock } from "../lib";
+import { createClock, defaultClockStep as currentTimeAnimation } from "../lib";
 
 createClock((clock) => {
-  clock.addCustomShape({
-    count: 24,
-    handler: async (index, instance) => {
-      return instance.createTextElement({
-        text: "hello",
-      });
+  clock.addHandle({
+    imageUrl: "./images/seconds.png",
+    scale: 0.1,
+    offsetY: -0.166,
+    label: "seconds",
+  });
+
+  clock.addHandle({
+    imageUrl: "./images/minutes.png",
+    scale: 0.1,
+    offsetY: -0.166,
+    label: "minutes",
+  });
+
+  clock.addHandle({
+    imageUrl: "./images/hours.png",
+    scale: 0.1,
+    offsetY: -0.23,
+    label: "hours",
+  });
+
+  clock.addAnimation({
+    duration: 3000,
+    handler: (progress, delta) => {
+      const step = currentTimeAnimation(clock);
+      step.handler?.(progress, delta);
     },
   });
 });
