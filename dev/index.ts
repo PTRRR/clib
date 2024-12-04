be7797b991c45846fcd3df4b61f02e2d7772249f
PTRRR <pietro.alberti@bluewin.ch>
2024-12-04 10:16:36 +0100

    Add custom radial gradient shader example

--- dev/index.ts
+++ dev/index.ts
@@ -1,4 +1,4 @@
-import { createClock, defaultFragmentHeader, RadialChart } from "../lib";
+import { createClock, defaultFragmentHeader } from "../lib";
 
 createClock((clock) => {
   const radius = clock.width * 0.5;
@@ -6,35 +6,16 @@ createClock((clock) => {
   const customFragmentShader = `
     ${defaultFragmentHeader}
 
-    uniform float uTime;
-
     void main() {
-      float dist = length(uv - vec2(0.5, 0.5)) * 2.0;
-      float waves = sin(dist * 1.5 * PI * 2.0 - uTime) * 0.1 * (1.0 - dist);
-      vec2 centeredUV = uv - vec2(0.5);
-      float angle = atan(centeredUV.y, centeredUV.x);
-      float normalizedAngle = (angle + PI) / (2.0 * PI) + waves;
-      float radialLines = abs((fract(normalizedAngle * 5.0) - 0.5)) * 2.0;
-      float halfChange = fwidth(radialLines) / 2.0;
-      float lowerEdge = 0.5 - halfChange * 1.5;
-      float upperEdge = 0.5 + halfChange * 1.5;
-      float step = smoothstep(lowerEdge, upperEdge, radialLines);
-      fragColor = vec4(mix(vec4(45.0 / 255.0, 0.0 / 255.0, 247.0 / 255.0, 1.0), vec4(59.0 / 255.0, 170.0 / 255.0, 64.0 / 255.0, 1.0), step));
+      vec2 newUv = rotateVec2(uv, PI * 0.5);
+      float gradient = getRadialGradient(newUv);
+      fragColor = vec4(gradient, gradient, gradient, 1.0);
     }
 `;
 
   clock.addRadialChart(new Array(100).fill(radius * 0.8), {
     subdivisions: 5,
     fragmentShader: customFragmentShader,
-    label: "chart",
-    resources: {
-      time: {
-        uTime: {
-          value: 0.0,
-          type: "f32",
-        },
-      },
-    },
   });
 
   clock.addRectangles({
@@ -52,19 +33,6 @@ createClock((clock) => {
         fontSize: 20,
         offset: 10,
       });
-    },
-  });
-
-  const chart = clock.getLayerByLabel("chart") as RadialChart;
-
-  clock.addAnimation({
-    duration: 5000,
-    handler: () => {
-      if (
-        typeof chart?.mesh.shader?.resources.time.uniforms.uTime === "number"
-      ) {
-        chart.mesh.shader.resources.time.uniforms.uTime += 0.01;
-      }
     },
   });
 });
